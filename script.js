c4af539e086641aef9383202b7432e1fb452db93
Karim <52040753+karimsiali@users.noreply.github.com>
2025-05-16 14:43:52 +0100

    Add few functions to remove/create grids
    I added a few functions and a button that prompts the user for a new
    grid size, the current grid is then removed and replaced with a new one
    with the size the user input (up to 100).

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 let container = document.querySelector(".container");
-let gridSize = 16;
+let btn = document.querySelector("button");
+let defaultSize = 16;
 let bgColor = "black";
 
-console.log("width is: "+container.style.width);
-for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-        let squareDiv = document.createElement("div");
-        squareDiv.setAttribute("class", "square");
-        squareDiv.style.width = (1 / gridSize * 100) + "%";
-        squareDiv.style.height = (1 / gridSize * 100) + "%";
-        container.appendChild(squareDiv);
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            let squareDiv = document.createElement("div");
+            squareDiv.setAttribute("class", "square");
+            squareDiv.style.width = (1 / gridSize * 100) + "%";
+            squareDiv.style.height = (1 / gridSize * 100) + "%";
+            container.appendChild(squareDiv);
+        }
     }
+    draw();
 }
 
-container.childNodes.forEach((div) => {
-    div.addEventListener("mouseenter", (e) => {
-        e.target.style.backgroundColor = bgColor;
+createGrid(defaultSize);
+
+function draw() {
+    container.childNodes.forEach((div) => {
+        div.addEventListener("mouseenter", (e) => {
+            e.target.style.backgroundColor = bgColor;
+        })
     })
+}
+
+function removeGrid() {
+    container.innerHTML = "";
+}
+
+
+
+btn.addEventListener("click", () => {
+    let newSize;
+    do {
+        newSize = prompt("Grid size");
+    } while (+newSize > 100);
+
+    removeGrid();
+    createGrid(newSize);
+    
 })
+
