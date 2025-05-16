a869336ee15b5674096bcf857739e583305fc15f
Karim <52040753+karimsiali@users.noreply.github.com>
2025-05-16 14:14:19 +0100

    Update the way size is calculated
    I changed the way the height and width of the 16x16 divs are calculated
    to make it more dynamic. I also changed a few variables and removed some
    unnecessary properties.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let container = document.querySelector(".container");
-let width = 16;
-let height = 16;
+let gridSize = 16;
 
-for (let i = 0; i < height; i++) {
-    for (let j = 0; j < width; j++) {
+console.log("width is: "+container.style.width);
+for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
         let squareDiv = document.createElement("div");
         squareDiv.setAttribute("class", "square");
+        squareDiv.style.width = (1 / gridSize * 100) + "%";
+        squareDiv.style.height = (1 / gridSize * 100) + "%";
         container.appendChild(squareDiv);
     }
 }
