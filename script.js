b9a24757727c911d8c4a490861348770d2b2cba6
Karim <52040753+karimsiali@users.noreply.github.com>
2025-05-17 12:40:32 +0100

    Remove unnecessary line

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function createGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
             let squareDiv = document.createElement("div");
-            squareDiv.setAttribute("class", "square");
             squareDiv.style.width = (1 / gridSize * 100) + "%";
             squareDiv.style.height = (1 / gridSize * 100) + "%";
             container.appendChild(squareDiv);
